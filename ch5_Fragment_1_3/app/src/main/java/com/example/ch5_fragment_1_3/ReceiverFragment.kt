1d1dd5a568186ec180dc2fe32336bff59ecb0efb
zzzzzuuuuu <agu0830@naver.com>
2024-04-25 13:18:24 +0900

    ch5: Fragment

---
+++ ch5_Fragment_1_3/app/src/main/java/com/example/ch5_fragment_1_3/ReceiverFragment.kt
@@ -0,0 +1,19 @@
+package com.example.ch5_fragment_1_3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ReceiverFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_receiver, container, false)
+    }
+
+}
